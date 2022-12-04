7b911cdb47830a7cea5e956cbae5b1f560493c2f
matthijs <matthijs@loftorbital.com>
2022-12-04 01:29:39 -0800

    feat: day4

--- day2/target/debug/deps/day2-d20cdc2d3d8179cb.d
+++
@@ -1,5 +0,0 @@
-/Users/mvd/Rust_Projects/aoc2022/day2/target/debug/deps/day2-d20cdc2d3d8179cb: src/main.rs
-
-/Users/mvd/Rust_Projects/aoc2022/day2/target/debug/deps/day2-d20cdc2d3d8179cb.d: src/main.rs
-
-src/main.rs:
