d26935a847c136e4d4201ad2d713fb808157ec67
matthijs <matthijs@loftorbital.com>
2022-12-05 22:21:20 -0800

    chore: edit header

---
+++ day6/target/debug/deps/day6-13a64db7da200559.d
@@ -0,0 +1,6 @@
+/Users/mvd/Rust_Projects/aoc2022/day6/target/debug/deps/day6-13a64db7da200559.rmeta: src/main.rs src/data/data.txt
+
+/Users/mvd/Rust_Projects/aoc2022/day6/target/debug/deps/day6-13a64db7da200559.d: src/main.rs src/data/data.txt
+
+src/main.rs:
+src/data/data.txt:
---
+++ day6/target/debug/deps/day6-7a0b62bac7ef3ef6.d
@@ -0,0 +1,6 @@
+/Users/mvd/Rust_Projects/aoc2022/day6/target/debug/deps/day6-7a0b62bac7ef3ef6.rmeta: src/main.rs src/data/data.txt
+
+/Users/mvd/Rust_Projects/aoc2022/day6/target/debug/deps/day6-7a0b62bac7ef3ef6.d: src/main.rs src/data/data.txt
+
+src/main.rs:
+src/data/data.txt:
