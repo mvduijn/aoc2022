be1ed825e7424a46f5c4ba591cb110041bffd04f
matthijs <matthijs@loftorbital.com>
2022-12-05 22:32:45 -0800

    refactor: cleanup

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -9,26 +9,18 @@ fn find_sequence (signal: &Vec<char>, of_size: usize) -> usize {
     let mut buffer: HashSet<char> = HashSet::with_capacity(of_size);
     let mut return_val: usize = 0;
 
-    for (index, _) in signal.iter().enumerate() {
+    for index in 0..signal.len() {
 
-        let items = signal.get(index..index + of_size);
-
-        if let Some(x) = items {
-
+        if let Some(x) = signal.get(index..index + of_size) {
             let existed: Vec<bool> = x.iter().map(|char| buffer.insert(*char)).collect();
-
             if !existed.contains(&false) {
                 return_val = index + of_size;
                 break
             }
         }
-
         buffer.clear()
-
     }
-
     return_val
-
 }
 
 fn main () -> () {
