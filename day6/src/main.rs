f1145f7f87c5759d26acb31695c39b25f4886bcb
matthijs <matthijs@loftorbital.com>
2022-12-05 23:20:48 -0800

    refactor: day6 optimizing

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -4,29 +4,28 @@ use std::{include_str, collections::HashSet};
 
 ////////////////////////////////////////////////////////////////////////////////
 
-fn find_sequence (signal: &Vec<char>, of_size: usize) -> usize {
+fn find_sequence (signal: &Vec<char>, of_size: usize) -> () {
 
     let mut buffer: HashSet<char> = HashSet::with_capacity(of_size);
-    let mut return_val: usize = 0;
 
     for index in 0..signal.len() {
 
         if let Some(x) = signal.get(index..index + of_size) {
-            let existed: Vec<bool> = x.iter().map(|char| buffer.insert(*char)).collect();
-            if !existed.contains(&false) {
-                return_val = index + of_size;
+            for char in x { buffer.insert(*char); }
+            if buffer.len() == of_size {
+                println!("Found sequence in array at position: {}", index + of_size);
                 break
             }
         }
+
         buffer.clear()
     }
-    return_val
 }
 
 fn main () -> () {
     let signal: Vec<char> = include_str!("data/data.txt").chars().collect();
-    println!("Part 1 value: {}", find_sequence(&signal, 4));
-    println!("Part 2 value: {}", find_sequence(&signal, 14));
+    find_sequence(&signal, 4);
+    find_sequence(&signal, 14);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
