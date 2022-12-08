d13a5d08cd21313016ece6b54d6ff2140d0858d3
matthijs <matthijs@loftorbital.com>
2022-12-08 01:41:26 -0800

    fix: day8 header

---
+++ day8/target/debug/deps/day8-a2c7f3439e9b2d8e.d
@@ -0,0 +1,6 @@
+/Users/mvd/Rust_Projects/aoc2022/day8/target/debug/deps/day8-a2c7f3439e9b2d8e.rmeta: src/main.rs src/data/data.txt
+
+/Users/mvd/Rust_Projects/aoc2022/day8/target/debug/deps/day8-a2c7f3439e9b2d8e.d: src/main.rs src/data/data.txt
+
+src/main.rs:
+src/data/data.txt:
